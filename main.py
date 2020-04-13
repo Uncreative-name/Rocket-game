49195228ec7bd9837b7ed59d5b967f931e50a466
Uncreative-name <gabriel.lasseter1@stu.pfisd.net>
2020-04-13 20:44:18 +0000

    V 1.1.3

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import pygame
+
 from pygame.locals import *
 
 pygame.init()
 
 screen_info =  pygame.display.Info()
-size = (width, height) = (int(screen_info.current_h), int(screen_info.current_h))
+size = (width, height) = (int(screen_info.current_w), int(screen_info.current_h))
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-color = (5, 29, 33)
+color = (0, 0, 0)
 screen.fill(color)
 
+class ship:
+  def __init__(self, pos):
+    super().__init__()
+    self.image = pygame.image.load("spaceRockets_003.png")
+    self.rect = self.image.get_rect()
+    self.image = pygame.transform.smoothscale(self.image, (int(0.4 * self.rect.width), int(0.4 * self.rect.height)))
+    self.rect = self.image.get_rect()
+    self.rect.center = pos
+
+start = (30,300)
+player = ship(start)
 
 def main():
   while True:
     clock.tick(60)
     screen.fill(color)
+    
+    screen.blit(player.image, player.rect)
+
     pygame.display.flip()
   
-
 if __name__ == "__main__":
   main()
